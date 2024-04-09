1c1eb98ea6755a45571017a53bf358ebc403d51f
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-04-09 17:56:21 +0900

    3 choix de prompt dans app.py

--- app.py
+++ app.py
@@ -10,13 +10,29 @@ st.image('Media/logo.png', width=200)
 
 st.title('Mon expert virtuel*')
 
-st.markdown('- C\'est un spécialiste en oncologie.  \n- Il aime répondre aux questions médicales du grand public, et ce, de façon imagée et synthétique.  \n - Il adore les métaphores.')
-st.markdown('*\* L\'expert virtuel n\'est pas médecin, il ne fournit que des renseignements d\'ordre général.  \nLes informations à caractère médical ne sont pas destinées à remplacer la consultation d\'un professionnel de la santé.*')
+profil_choice = st.radio('Choisis ton expert :', ('Le professeur des écoles (*pour les enfants*)', 'Le journaliste (*synthétique*)', 'Le scientifique (*technique*)'))
+
+if profil_choice == 'Le professeur des écoles (*pour les enfants*)':
+    profil = 'Tu es un médecin. Tu t\'adresses à des enfants et fais des réponses très imagées en utilisant notamment des métaphores. Tu réponds en 5 lignes maximum. Tu ne fais que répondre aux questions sans faire de fausses promesses aux patients.'
+    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime répondre aux questions médicales du grand public, et ce, de façon **imagée** et synthétique.  \n - Il adore les **métaphores**.')
+    st.markdown('*\* L\'expert virtuel n\'est pas médecin, il ne fournit que des renseignements d\'ordre général.  \nLes informations à caractère médical ne sont pas destinées à remplacer la consultation d\'un professionnel de la santé.*')
+
+elif profil_choice == 'Le scientifique (*technique*)':
+    profil = 'Tu es un médecin. Tu ne réponds qu\'à des questions en lien avec la médecine. Tu détailles au maximum tes réponses. Tu ne fais que répondre aux questions sans faire de fausses promesses aux patients.'
+    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime aller dans le coeur d\'un sujet et partager le maximum de **détails**.  \n - Il ne répond qu\'aux questions médicales.')
+    st.markdown('*\* L\'expert virtuel n\'est pas médecin, il ne fournit que des renseignements d\'ordre général.  \nLes informations à caractère médical ne sont pas destinées à remplacer la consultation d\'un professionnel de la santé.*')
+
+else :
+    profil = 'Tu es un médecin. Tu ne réponds qu\'à des questions en lien avec la médecine. Tu réponds en 5 lignes maximum dans un style journalistique si cela est pertinent.'
+    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime **synthétiser** les concepts en quelques lignes quand cela est possible.  \n - Il ne répond qu\'aux questions médicales.')
+    st.markdown('*\* L\'expert virtuel n\'est pas médecin, il ne fournit que des renseignements d\'ordre général.  \nLes informations à caractère médical ne sont pas destinées à remplacer la consultation d\'un professionnel de la santé.*')
+
 st.markdown("""#### A votre tour maintenant, posez vos questions !""")
 
-st.markdown('Quelques exemples :  \n*Peut-on guérir de tous les cancers ?*  \n*Qu\'est-ce qui distingue une cellule saine d\'une cellule métastatique ?*')
+st.markdown('Quelques exemples :  \n*Pourquoi les os sont durs ?*  \n*Comment fabrique-t-on un vaccin ?*  \n*Qu\'est-ce qui distingue une cellule saine d\'une cellule métastatique ?*')
 
 client = OpenAI()
+
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -38,7 +54,7 @@ if prompt := st.chat_input("Posez votre question dans ce chat"):
             model=st.session_state["openai_model"],
 
             messages=[
-                {"role": "system", "content": 'Tu es un médecin oncologue, professeur des université et spécialiste des cancers. Tu fais des réponses imagées et tu utilises de temps en temps des métaphores, et tu réponds en 5 lignes maximum. Tu ne fais que répondre aux questions sans faire de fausses promesses aux patients.'},
+                {"role": "system", "content": profil},
                 {"role": "user", "content": prompt}
             ],
 
