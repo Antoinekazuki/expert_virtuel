cb0042a77d7c9e27c9e565adaf0ac6102ec3e5d3
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-04-09 14:17:38 +0900

    set display mode on 'wide'

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from openai import OpenAI
 import streamlit as st
 
+def wide_space_default():
+    st.set_page_config(layout='wide')
+
+wide_space_default()
+
 st.image('Media/logo.png', width=200)
 
 st.title('Mon médecin virtuel')
