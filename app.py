51dc3765a396e0d424207260e7835fa7e643c213
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-04-09 14:10:23 +0900

    nouveau logo et avatar ajoutés, modifications de app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title('Je discute avec mon PDS')
+st.image('Media/logo.png', width=200)
 
-# Set OpenAI API key from Streamlit secrets
+st.title('Mon médecin virtuel')
+
+st.markdown('- Médecin spécialisé en oncologie.  \n- Il aime répondre aux questions médicales du grand public, et ce, de façon imagée et synthétique.  \n - Il adore les métaphores.')
+
+st.markdown("""#### A votre tour maintenant, posez vos questions au médecin virtuel !""")
+st.markdown('Quelques exemples :  \n*Peut-on guérir de tous les cancers ?*  \n*Qu\'est-ce qui distingue une cellule saine d\'une cellule métastatique ?*')
+
 client = OpenAI()
 
 if "openai_model" not in st.session_state:
@@ -14,23 +20,23 @@ if "messages" not in st.session_state:
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Posez votre question dans ce chat"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    with st.chat_message("assistant"):
+    with st.chat_message("system", avatar='🧑🏻‍⚕️'):
         stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
 
             messages=[
-                {"role": "system", "content": 'Tu es un médecin oncologue, professeur des université et spécialiste des cancers. Tu fais des réponses très imagées qui utilise des métaphores, et tu réponds en 5 lignes maximum.'},
+                {"role": "system", "content": 'Tu es un médecin oncologue, professeur des université et spécialiste des cancers. Tu fais des réponses imagées et tu utilises de temps en temps des métaphores, et tu réponds en 5 lignes maximum.'},
                 {"role": "user", "content": prompt}
             ],
 
             stream=True,
         )
         response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "system", "content": response})
