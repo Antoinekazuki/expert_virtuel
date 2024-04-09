cef98c8b7e5c8effaf57a59ad6cbf7e3192a86e0
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-04-09 18:01:44 +0900

    optimisation afficahge 2

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ profil_choice = st.radio('Choisis ton expert :', ('Le professeur des écoles (*p
 
 if profil_choice == 'Le professeur des écoles (*pour les enfants*)':
     profil = 'Tu es un médecin. Tu t\'adresses à des enfants et fais des réponses très imagées en utilisant notamment des métaphores. Tu réponds en 5 lignes maximum. Tu ne fais que répondre aux questions sans faire de fausses promesses aux patients.'
-    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime répondre aux questions médicales du grand public, et ce, de façon **imagée** et synthétique.  \n - Il adore les **métaphores**.')
+    st.markdown('- C\'est un spécialiste de la médecine qui aime répondre aux questions médicales du grand public, et ce, de façon **imagée** et synthétique.  \n - Il adore les **métaphores**.')
     st.markdown(disclaimer_text)
 
 elif profil_choice == 'Le scientifique (*technique*)':
     profil = 'Tu es un médecin. Tu ne réponds qu\'à des questions en lien avec la médecine. Tu détailles au maximum tes réponses. Tu ne fais que répondre aux questions sans faire de fausses promesses aux patients.'
-    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime aller dans le coeur d\'un sujet et partager le maximum de **détails**.  \n - Il ne répond qu\'aux questions médicales.')
+    st.markdown('- C\'est un spécialiste de la médecine qui aime aller dans le coeur d\'un sujet et partager le maximum de **détails**.  \n - Il ne répond qu\'aux questions médicales.')
     st.markdown(disclaimer_text)
 
 else :
     profil = 'Tu es un médecin. Tu ne réponds qu\'à des questions en lien avec la médecine. Tu réponds en 5 lignes maximum dans un style journalistique si cela est pertinent.'
-    st.markdown('- C\'est un spécialiste de la médecine.  \n- Il aime **synthétiser** les concepts en quelques lignes quand cela est possible.  \n - Il ne répond qu\'aux questions médicales.')
+    st.markdown('- C\'est un spécialiste de la médecine qui aime **synthétiser** les concepts en quelques lignes quand cela est possible.  \n - Il ne répond qu\'aux questions médicales.')
     st.markdown(disclaimer_text)
 
 st.markdown("""#### A votre tour maintenant, posez vos questions !""")
