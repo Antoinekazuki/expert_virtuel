629c6f7678958a3607661df187858fc49d2b9341
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-04-09 12:32:34 +0900

    app.py 1ères transformations pour en faire un chat bot

--- app.py
+++ app.py
@@ -1,51 +1,36 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title('GPT test')
+st.title('Je discute avec mon PDS')
 
-def generate_response(input_text):
-    client = OpenAI()
-
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": 'Tu es un médecin oncologue, professeur des université et spécialiste des cancers. Tu fais des réponses très imagées qui utilise des métaphores, et tu réponds en 5 lignes maximum.'},
-        {"role": "user", "content": input_text}
-    ]
-    )
-
-    return completion.choices[0].message
-
-
-with st.form('my_form'):
-    text = st.text_area('Enter text:', 'Posez votre question ici.')
-    submitted = st.form_submit_button('Submit')
-    if submitted:
-        st.markdown(generate_response(text))
-
-
+# Set OpenAI API key from Streamlit secrets
 client = OpenAI()
 
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-#   ]
-# )
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# print(completion.choices[0].message)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-# def generate_response(input_text):
-#     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-#     st.info(llm(input_text))
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
 
-# with st.form('my_form'):
-#     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-#     submitted = st.form_submit_button('Submit')
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     if submitted and openai_api_key.startswith('sk-'):
-#         generate_response(text)
+            messages=[
+                {"role": "system", "content": 'Tu es un médecin oncologue, professeur des université et spécialiste des cancers. Tu fais des réponses très imagées qui utilise des métaphores, et tu réponds en 5 lignes maximum.'},
+                {"role": "user", "content": prompt}
+            ],
+
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
